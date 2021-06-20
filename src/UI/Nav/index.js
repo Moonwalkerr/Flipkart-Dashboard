3c8398ad0d28881df835630047dee7dfcac5bfd2
Moonwalker <abhisk.mhra@gmail.com>
2021-06-20 12:21:00 +0530

    removed styles.css and added makeStyles theme to the page

--- src/UI/Nav/index.js
+++ src/UI/Nav/index.js
@@ -2,28 +2,44 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Button } from "@material-ui/core";
 import { ArrowDropDown } from "@material-ui/icons";
 import CustomizedInputBase from "./searchInput";
-import "./styles.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
     alignItems: "center",
-    width: 600,
+    padding: 7,
+    backgroundColor: "#2874f0",
+    maxHeight: 41,
+    minHeight: 41,
+    color: "#fafafafa",
+    display: "flex",
+  },
+  navbarLogo: {
+    marginRight: 20,
+    fontStyle: "italic",
+    fontSize: 22,
+    display: "flex",
+    flexDirection: "column",
   },
   login: {
+    backgroudColor: "#fafafafa",
     cursor: "pointer",
     display: "flex",
     alignItems: "center",
     marginLeft: 10,
-    // fontWeight: "bold",
+  },
+  innerHeading: {
+    fontSize: 11,
+  },
+  plus: {
+    color: "yellow",
   },
 
   dropDown: {
+    color: "#fafafafa",
     cursor: "pointer",
     display: "flex",
-    alignItems: "center",
-    marginLeft: 10,
-    color: "#fafafafa",
     fontWeight: "bold",
   },
 }));
@@ -32,17 +48,15 @@ const Nav = () => {
   const classes = useStyles();
 
   return (
-    <header className="navbar">
-      <h3>
+    <header className={classes.root}>
+      <h3 className={classes.navbarLogo}>
         Flipkart
-        <div className="innerHeading">
-          Explore <span id="plus">Plus</span>
+        <div className={classes.innerHeading}>
+          Explore <span className={classes.plus}>Plus</span>
         </div>
       </h3>
       <CustomizedInputBase />
-      <Button variant="contained" className={classes.login}>
-        Login
-      </Button>
+      <Button className={classes.login}>Login</Button>
       <Button className={classes.dropDown}>
         More
         <ArrowDropDown />

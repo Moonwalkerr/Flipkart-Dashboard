204655d8cb8a8f5b8d25ebe7a08b77e55efdc92a
Moonwalker <abhisk.mhra@gmail.com>
2021-06-20 16:17:28 +0530

    removed  helper function, not needed right now

--- src/helpers/functions.js
+++
@@ -1,6 +0,0 @@
-// this function fetches data from any json file, and sets the data to a particular state using setter
-export const getData = async (path, setter) => {
-  let res = await fetch(path);
-  let data = await res.json();
-  return setter(data);
-};

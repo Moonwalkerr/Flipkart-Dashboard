0ffaeb1a6de4579327370367d18436548790b052
Moonwalker <abhisk.mhra@gmail.com>
2021-06-20 15:51:18 +0530

    getData imported and used to prodListGird

--- src/UI/ProdList/index.js
+++ src/UI/ProdList/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { getData } from "../../helpers/functions";
 
 const DATAPATH = "../../data/db.json";
 
@@ -6,9 +7,13 @@ const ProdListGrid = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    getData(DATAPATH);
+    getData(DATAPATH, setData);
   }, []);
-  // return (  );
+  return (
+    <div className="ProdListGrid">
+      {data.length > 0 ? <h1>Data</h1> : <h2>Loading....</h2>}
+    </div>
+  );
 };
 
 export default ProdListGrid;

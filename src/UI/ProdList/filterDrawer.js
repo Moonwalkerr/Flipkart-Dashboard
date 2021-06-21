a07e0b8f4de6285aeb80e72045bd915d48d42ce7
Moonwalker <abhisk.mhra@gmail.com>
2021-06-21 20:31:59 +0530

    Slider Filter by price implementation done

--- src/UI/ProdList/filterDrawer.js
+++ src/UI/ProdList/filterDrawer.js
@@ -2,11 +2,10 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Slider } from "@material-ui/core";
 import { useContext, useState } from "react";
 import { AppContext } from "../../context/context";
-
+import db from "../../data/db.json";
 // stylings for the Navbar via material ui stylings
 const useStyles = makeStyles((theme) => ({
   root: {
-    flex: 0.45,
     padding: 20,
   },
 }));
@@ -18,19 +17,16 @@ const FilterDrawer = () => {
   // fetching displayData state from Context layer
   const [value, setValue] = useState([100, 100000]);
   const [displayData, setDisplayData] = useContext(AppContext).displayData;
-  const [preValue, setPreValue] = useState(value);
-  const [nextValue, setNextValue] = useState(null);
 
   //   slider event change handler
   const handleChange = (evt, newValue) => {
-    // setpreValue(value);
     setValue(newValue);
-    setNextValue(newValue);
+    setDisplayData(sortData(newValue[0], newValue[1]));
+    console.log(displayData);
   };
 
-  function sortData(preValue, newValue) {
-    displayData.filter((x) => x.Price >= preValue && x.Price <= nextValue);
-  }
+  const sortData = (preValue, newValue) =>
+    db.filter((data) => data.price >= preValue && data.price <= newValue);
   return (
     <div className={classes.root}>
       <h4>Check via Price Range</h4>

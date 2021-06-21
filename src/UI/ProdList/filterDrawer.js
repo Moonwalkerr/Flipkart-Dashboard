294700cd4bf1d3c155742e1a5747f96e0f3b7ed6
Moonwalker <abhisk.mhra@gmail.com>
2021-06-21 17:52:38 +0530

    Filtering method WIP

--- src/UI/ProdList/filterDrawer.js
+++ src/UI/ProdList/filterDrawer.js
@@ -1,6 +1,7 @@
 import { makeStyles } from "@material-ui/core/styles";
 import { Slider } from "@material-ui/core";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AppContext } from "../../context/context";
 
 // stylings for the Navbar via material ui stylings
 const useStyles = makeStyles((theme) => ({
@@ -14,15 +15,22 @@ const FilterDrawer = () => {
   // fetching classes from useStyles()
   const classes = useStyles();
 
+  // fetching displayData state from Context layer
   const [value, setValue] = useState([1, 100]);
-  // const [preValue, setPreValue] = useState(value);
-  // const [nextValue, setNextValue] = useState(null);
+  const [displayData, setDisplayData] = useContext(AppContext).displayData;
+  const [preValue, setPreValue] = useState(value);
+  const [nextValue, setNextValue] = useState(null);
 
   //   slider event change handler
   const handleChange = (evt, newValue) => {
+    // setpreValue(value);
     setValue(newValue);
-    // setNextValue(newValue);
+    setNextValue(newValue);
   };
+
+  function sortData(preValue, newValue) {
+    displayData.filter((x) => x.Price >= preValue && x.Price <= nextValue);
+  }
   return (
     <div className={classes.root}>
       <h4>Check via Price Range</h4>
